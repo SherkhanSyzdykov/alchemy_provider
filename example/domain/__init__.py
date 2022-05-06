946df960f3f00a35e2cefaf067b435b006cf5f37
Sherkhan <syzdykovsherkhan@gmail.com>
2022-05-06 15:58:29 +0600

    deleted unnecessary modules

--- example/domain/__init__.py
+++
@@ -1,2 +0,0 @@
-from .device import *
-from .device_type import *
--- example/domain/base.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from query import AbstractQuery
-
-
-class BaseDomain(BaseModel, AbstractQuery):
-    pass
